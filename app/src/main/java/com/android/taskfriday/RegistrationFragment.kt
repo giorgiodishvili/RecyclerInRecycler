552a7e1520d3a8c4438d8bf9091029747564e30e
giorgi <giorgiodishvili>
2021-06-04 12:01:36 +0400

    todo check

--- app/src/main/java/com/android/taskfriday/RegistrationFragment.kt
+++ app/src/main/java/com/android/taskfriday/RegistrationFragment.kt
@@ -16,6 +16,7 @@ class RegistrationFragment : Fragment() {
     private lateinit var adapter: ParentAdapter
     private lateinit var binding: RegistrationFragmentBinding
     private val viewModel: RegistrationViewModel by viewModels()
+    private var size: Int =0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,13 +42,19 @@ class RegistrationFragment : Fragment() {
     private fun observe(){
         viewModel._fetchedFields.observe(viewLifecycleOwner,{
             Log.d("isSuccesfful", "${it[0]}")
+            size = it.size
             adapter.setData(it)
         })
     }
 
+    //TODO check
     fun init(){
         binding.Register.setOnClickListener{
-
+            for(i in 0..size){
+                val findViewHolderForLayoutPosition =
+                    binding.recyclerView.findViewHolderForLayoutPosition(i)
+//                findViewHolderForLayoutPosition.itemView.
+            }
         }
     }
 
